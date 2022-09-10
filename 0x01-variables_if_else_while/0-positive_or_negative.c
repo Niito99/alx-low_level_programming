31fa4e80a9ed8260779fb21e238d0b0a87327232
Kali <kali@kali>
2022-09-10 00:15:34 -0400

    Add code for random num gen

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,20 +4,22 @@
 
 /* 
  * The code assignsn a random number to n 
- * Everytime the code is run ; the random number is printed out; and wether it is negative or positive is also printed out
+ * Everytime the code is run ;
+ * the random number is printed out;
+ *  and wether it is negative or positive is also printed out
  * The main function returns zero
- * */
+  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
+	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n==0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if (n<0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
 }
