3a53cee5af5007bd30327fd383de048eac2aaf06
Kali <kali@kali>
2022-09-13 00:09:10 -0400

    Add code using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints a string when executed
  * Returns: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
