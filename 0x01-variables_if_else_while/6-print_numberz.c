c8df8fc5d1614c972b81f5a881dd966e291d9080
Kali <kali@kali>
2022-09-13 01:46:28 -0400

    Prints digits using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digits numbers of base 10
+ * starting from zero using putchar
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
