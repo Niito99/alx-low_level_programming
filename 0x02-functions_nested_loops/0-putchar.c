98ceb2c4685ffdefb014879f4dc2a7822674b50f
Kali <kali@kali>
2022-09-14 01:00:45 -0400

    Prints out a string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the string _putchar followed by a newline
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_putchar');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This dir contains c files for nested loops and knowledge on header files.
+0. the first file prints out the string _putchar.
