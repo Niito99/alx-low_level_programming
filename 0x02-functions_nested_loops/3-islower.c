3f3af7bc382e82f65c244882eb55e7d5a17e57c0
Kali <kali@kali>
2022-09-14 01:43:30 -0400

    Checks for lowercase alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - checks if alphabet is lowercase or not
+ * Return: 1 if lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	int num;
+
+	if (c >= 'a' && c <= 'z')
+	{
+		num = 1;
+	}
+	else
+	{
+		num = 0;
+	}
+	return (num);
+}
