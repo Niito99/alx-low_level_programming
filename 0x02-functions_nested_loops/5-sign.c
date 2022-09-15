a4ee9d2cb24637eecd255ff247ba8ccc77fa52e1
Kali <kali@kali>
2022-09-14 22:43:14 -0400

    Checks wether a number is positive or negative

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		num = 1;
-		putchar("+");
+		_putchar("+");
 	}
 	else if (n == 0)
 	{
 		num = 0;
-		putchar("0");
+		_putchar("0");
 	}
 	else
 	{
 		num = -1;
-		putchar("-");
+		_putchar("-");
 	}
 	return (num);
 }
