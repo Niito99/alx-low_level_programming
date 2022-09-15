af84e952c5ec6513f989da74cd2f7642232a84ee
Kali <kali@kali>
2022-09-15 01:04:08 -0400

    Sums even num in a fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,19 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - prints the sum of the even valued terms
  * Return: the sum of the terms
  */
 
-int main(int n)
+int main(void)
 {
+	int n;
+
+	scanf("%d", &n);
 	long i = 0;
 	long term1 = 1;
 	long term2 = 2;
 	long next = term1 + term2;
 
-	int sum = 0;
+	long int sum = 0;
 
 	for (i = 0; i <= n; n++)
 	{
