faa23943619b183bc39b240f9826eae9ac891736
Kali <kali@kali>
2022-09-14 22:56:26 -0400

    Finds the abs of a num

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * Return: the absolute value of the integer
+ */
+
+int _abs(int i)
+{
+	int abs;
+
+	if (i > 0)
+	{
+		abs = i;
+	}
+	else if (i < 0)
+	{
+		abs = i * -1;
+	}
+	else
+	{
+		abs = 0;
+	}
+	return (abs);
+}
