22a3700a1685df4cc07912888d4c955b56966395
Kali <kali@kali>
2022-09-14 23:50:36 -0400

    Prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int n = 0;
+	int num;
+  while (n <= '9')
+	{
+		for (num = 0; num <= 9; num++)
+		{
+			int x = num * n;
+			return(x);
+			_putchar(',');
+		}
+		n++;
+		num = '0';
+	}
+}
