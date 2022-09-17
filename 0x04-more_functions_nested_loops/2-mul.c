3937c7d0c58f06a70d781ac4b1f1735ed2a5aa96
Kali <kali@kali>
2022-09-16 23:37:51 -0400

    update code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+
+#include "main.h"
+/**
+ * mul - this function multiplies two values
+ *
+ * @a: character value
+ * @b: character value
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+
+}
