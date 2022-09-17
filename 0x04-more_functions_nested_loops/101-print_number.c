add939a23b48467d56e4a24a932887ac5bd02117
Kali <kali@kali>
2022-09-16 23:52:42 -0400

    update code

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,48 +1,37 @@
 #include "main.h"
-
-void print_prev_digits(int n);
-
 /**
- * print_number - prints a number using _putchar
- *
- * @n: number to print
- * Return: void
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
  */
+
 void print_number(int n)
 {
+
+	int d = 1, i = 0, ii = 0;
+
 	if (n < 0)
 	{
 		_putchar('-');
+		n = -n;
 	}
-	if (n > 9 || n < -9)
-		print_prev_digits(n);
-	if (n < 0)
-		_putchar(((n % 10) * -1) + '0');
-	else
-		_putchar((n % 10) + '0');
-}
 
-/**
- * print_prev_digits - recursive function to print all previous digits in num
- *
- * @n: number to print previous digits of
- * Return: void
- */
-void print_prev_digits(int n)
-{
-	if (((n / 10) < -9) || ((n / 10) > 9))
+	while (n / d != 0)
 	{
-		print_prev_digits(n / 10);
-		if (n < 0)
-			_putchar((((n / 10) % 10) * -1) + '0');
-		else
-			_putchar(((n / 10) % 10) + '0');
+		d *= 10;
+		i++;
 	}
-	else
+	d = d / 10;
+
+	while (ii < i)
 	{
-		if (n < 0)
-			_putchar(((n / 10) * -1) + '0');
-		else
-			_putchar((n / 10) + '0');
+		_putchar('0' + n / d);
+		n = n - (n / d) * d;
+		d = d / 10;
+		ii++;
 	}
+
+	if (i == 0)
+		_putchar('0' + n);
+
 }
