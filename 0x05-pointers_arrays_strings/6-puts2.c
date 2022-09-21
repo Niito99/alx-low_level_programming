d64debf9b3a33c6ac7805e7e2f4f2a95833885fd
Kali <kali@kali>
2022-09-20 23:04:41 -0400

    Prints every other char

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: argument of string type passed
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[i] != '\0')
+	{
+		i++
+		j += 1
+	}
+	j -= 1;
+	
+	for (i = 0; i < j; i++)
+	{
+		if ( i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+}
