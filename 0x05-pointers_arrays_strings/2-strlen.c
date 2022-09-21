803f9716257e6d590f1c0e5bb78f8517e66af37f
Kali <kali@kali>
2022-09-20 21:34:40 -0400

    Returns the length of the  string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 
 /**
  * _strlen - finds the length of the string
- * @s: the char passed as a parameter 
+ * @s: the char passed as a parameter
  * Return: the length of the string
  */
 
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i])
 	{
 		i++;
