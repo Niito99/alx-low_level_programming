e8925f08882fa48c35c963dd02ae3a0d707d2804
Kali <kali@kali>
2022-09-20 20:46:01 -0400

    Write func that swaps value of 2 int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * swap_int - interchanges the values of two integers that
+ * are passed int as arguments
+ * @a: integer value of first argument
+ * @b: integer value of second argument
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	int d;
+
+	a = c;
+	b = d;
+	*a = d;
+	*b = c;
+}
