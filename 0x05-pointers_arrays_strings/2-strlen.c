524403574b6fe6a6f823e0e0e488831541088b3f
Kali <kali@kali>
2022-09-20 21:19:34 -0400

    Returns the length of the  string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - prints the length of a string
+ * @s: any character passed as an argument
+ * Return: the lenght of the string in int
+ */
+
+int _strlen(char *s)
+{
+	strlen(*s);
+}
