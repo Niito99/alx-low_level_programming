85d6d6f6902825966496600a92fb50e626f2a943
Kali <kali@kali>
2022-09-20 20:36:00 -0400

    Write func that takes pointer and updates it

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * reset_to_98 - updates the value passed as an argument to 98
- * @*n: pointer to an integer n
+ * @n: pointer to an integer n
  * Return: returns void
  */
 
 void reset_to_98(int *n)
 {
 	int p;
+
 	p = 98;
 	*n = p;
 }
