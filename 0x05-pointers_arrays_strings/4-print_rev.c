14c57a76532b9deeea06043d63f0815f9599a9db
Kali <kali@kali>
2022-09-20 21:57:07 -0400

    prints char in reverse using while loop

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: param of str type passed
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = strlen(s) - 1;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
