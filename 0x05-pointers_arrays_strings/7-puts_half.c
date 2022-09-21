28f7323231b291407b0cb3f8f0ead24610985b67
Kali <kali@kali>
2022-09-20 23:35:58 -0400

    prints the last half of a char

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * puts_half - prints only half of the input char
+ * @str: param passed to the func
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int len;
+
+	while (str[i] != '\0')
+	{
+		j++;
+		i++;
+	}
+	if (j % 2 == 0)
+	{
+		len = j / 2;
+		for (k = len; k < j - 1; k++)
+		{
+			_putchar(str[k]);
+		}
+	}
+	else
+	{
+		len = (j - 1) / 2;
+		for (k = len; k < j - 1; k++)
+		{
+			_putchar(str[k]);
+		}
+	}
+}
