4f4b80cc48221efd760715b87aad3d0022f25fbc
Kali <kali@kali>
2022-09-22 13:41:44 -0400

    Concatenates two strings at n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * *_strncat - appends second string (at n bytes) to
+ * the first string
+ * @dest: first param pointer value
+ * @src: pointer to the second string
+ * @n: param reping the value of byte to append
+ * Return: pointer value of dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j <= n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+
+	}
+	dest[i] = '\0';
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 This readme file contains more code on pointers arrays and strings
 The following are what each task numbered n does
 0. writes a function that concatenates two strings
+1. writes a function that appends the second string to the first one(at n bytes); meaning not more than n bytes of the string n
