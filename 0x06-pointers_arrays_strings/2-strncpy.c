4f55d8524248bf91af582645d520af359899c2f2
Kali <kali@kali>
2022-09-22 14:46:57 -0400

    Copies one string into another var

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,22 @@
 #include "main.h"
-/*
+/**
  * _strncpy - copies a string into another variable
  * @dest: the destination of the copied string
  * @src: the char value to be copied
  * @n: the number of chars to copy
- * Return: dest
+ * Return: dest var
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	i = 0;
-
 	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	for (; i < n; i++)
+	for (;i < n; i++)
 	{
 		dest[i] = '\0';
 	}
