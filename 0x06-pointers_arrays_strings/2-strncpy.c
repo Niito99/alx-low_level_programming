c2a0ce217c22be969bbf623236d75e8b4edfbfdb
Kali <kali@kali>
2022-09-22 14:34:13 -0400

    Copies one string into another var

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,13 +6,11 @@
  * @n: the number of chars to copy
  * Return: dest var
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
 	i = 0;
-
 	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
@@ -22,6 +20,5 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = '\0';
 	}
-
 	return (dest);
 }
