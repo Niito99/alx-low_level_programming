eda822257864abeda507ec8cda4f2deef2412c5b
Kali <kali@kali>
2022-09-22 14:12:49 -0400

    Copies one string into another var

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string into another variable
+ * @dest: the destination of the copied string
+ * @src: the char value to be copied
+ * @n: the number of chars to copy
+ * Return: dest var
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	i = 0;
+
+	while (src[i] != '\0' && i <= n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -2,3 +2,4 @@ This readme file contains more code on pointers arrays and strings
 The following are what each task numbered n does
 0. writes a function that concatenates two strings
 1. writes a function that appends the second string to the first one(at n bytes); meaning not more than n bytes of the string n
+2. Task contain a function that copies one string and stores in another variable 
