9032714e0cc24e1b521ae320a8606ca138c1e3c6
Kali <kali@kali>
2022-09-22 23:49:02 -0400

    Reverses an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_array - reverses the contents of an array of ints
+ * @a: the array that would be reversed
+ * @b: the number of elements of the array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	for (i = n - 1; i >= 1; i--)
+	{
+		printf("%d", a[i]);
+	}
+	return ;
+}
