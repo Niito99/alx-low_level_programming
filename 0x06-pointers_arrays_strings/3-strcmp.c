d47a55bd476bf3b5e33a8b21cf69af9883a1b76d
Kali <kali@kali>
2022-09-22 23:15:52 -0400

    Compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strcmp - function compares two strings
+ * @s1: the first string parameter
+ * @s2: the second string parameter
+ * Return: 0 if strings are equal but >0 if the first
+ * non matching character in str1 is greater in ASCII
+ * than the first in s2. and <0 if otherwise
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	int result = 0;
+
+	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
+	{
+		i++;
+	}
+	if (s1[i] == s2[i])
+	{
+		result = 0;
+	}
+	else (s1[i] != s2[i])
+	{
+		result = s1[i] - s2[i];
+	}
+	return (result);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -3,3 +3,4 @@ The following are what each task numbered n does
 0. writes a function that concatenates two strings
 1. writes a function that appends the second string to the first one(at n bytes); meaning not more than n bytes of the string n
 2. Task contain a function that copies one string and stores in another variable 
+3. the task writes a function that compares two strings
