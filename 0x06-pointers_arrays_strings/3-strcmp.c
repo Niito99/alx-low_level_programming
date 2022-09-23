3b81b1459f5e8c6c78f837a37b32ddc1e8111bab
Kali <kali@kali>
2022-09-22 23:31:47 -0400

    Compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,20 +13,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	i = 0;
-	int result = 0;
-
-	while ((s1[i] != '\0' && s2[i] != '\0') && s1[i] == s2[i])
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		i++;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-	if (s1[i] == s2[i])
-	{
-		result = 0;
-	}
-	else
-	{
-		result = s1[i] - s2[i];
-	}
-	return (result);
+	return (0);
 }
