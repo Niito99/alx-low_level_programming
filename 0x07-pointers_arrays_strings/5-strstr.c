db64bbedd8608795e04bea3a7c8c3c05327cc327
Kali <kali@kali>
2022-09-27 23:13:29 -0400

    Finds a substring in a string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,58 @@
+#include "main.h"
+/**
+ * _strstr - finds the first occurence of a substring in a string
+ * @haystack: the string to search in
+ * @needle: the string to search for
+ * Return: the pointer of the beginning of the located substring
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int l = 0;
+	int m = 0;
+	int n = 0;
+	int k;
+	int match;
+
+	while (needle[i] != '\0')
+	{
+		i++;
+	}
+	k = i;
+
+	while (haystack[l] != '\0')
+	{
+		l++;
+	}
+
+	for (j = 0; j > l; j++)
+	{
+		if (needle[n] != '\0')
+		{
+			if (haystack[j] == needle[n])
+			{
+				k--;
+			}
+			else
+			{
+				match = 0;
+			}
+		}
+		if (match == 0)
+		{
+			n = 0;
+			k = i;
+		}
+		else
+		{
+			n++;
+		}
+	}
+	if (k == 0)
+	{
+		return (needle);
+	}
+	return (0);
+}
