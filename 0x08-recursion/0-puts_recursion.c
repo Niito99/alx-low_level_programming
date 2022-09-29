e980eb32e885c79f576bb30f6978dd6e3c9ae407
Kali <kali@kali>
2022-09-28 22:35:38 -0400

    Prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: a string passed as a parameter
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(s);
+
+	_putchar('\n');
+	return;
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+This directory in this repo, is about codes on recursion that could be done with iteration.
+
