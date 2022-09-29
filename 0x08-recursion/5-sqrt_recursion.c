0c9d486f081beb55486b3067dc626ab7794bf2a0
Kali <kali@kali>
2022-09-29 00:39:41 -0400

    Update all

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * _sqrt_recursion - finds the sqrt of a number
- *
  * @n: number passed into function as parameter
+ *
  * find_sqrt - checks if a  num is a square root
  * @num: the number being checked
  * @root: the root being checked for
+ *
+ *
  * Return: the natural square root of n
  */
-int find_sqrt(int num, int root);
-int _sqrt_recursion(int n);
 
 int find_sqrt(int num, int root)
 {
