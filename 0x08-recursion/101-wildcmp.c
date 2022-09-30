7975114b820cd9c8047a8c0916e28cd5410e3d86
Kali <kali@kali>
2022-09-30 00:16:39 -0400

    Palindrome implies reverse is forward

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * wildcmp - compares two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: 1 if the strings are similar and 0 if they arent
+ */
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,3 +5,5 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int square(int n, int val);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
