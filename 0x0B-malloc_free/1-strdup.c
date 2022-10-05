466631e424bc48a7784b278d33b5624df57da52f
Kali <kali@kali>
2022-10-05 00:10:23 -0400

    Returns pointer to newly allocated mem

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicates the content of a newly allocated pointer to a string
+ * @str: string passed as param
+ * Return: pointer to a string
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	dup = malloc(sizeof(dup));
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+	return (dup);
+}
