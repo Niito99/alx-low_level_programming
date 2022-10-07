283dcd99c366e7dab9f8b3ab3b2cdd14a18a7939
Kali <kali@kali>
2022-10-07 16:29:42 -0400

    Creates an array of ints

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,13 +10,14 @@
 
 int *array_range(int min, int max)
 {
+	int *list;
 	int i;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	int list[] = malloc(sizeof(int) * (max - min) + 1);
+	list = malloc(sizeof(int) * ((max - min) + 1));
 	if (list == NULL)
 	{
 		return (NULL);
