20292e9c4110f77a5a3be7edcd36bb6d396f87bd
Kali <kali@kali>
2022-10-24 22:48:04 -0400

    More code on singly linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_listint - prints the items in a singly linked list
+ * @h: pointer to the first node of the singly linked list
+ * Return: the number of nodes in the linked list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	listint_t start;
+	int i = 0;
+
+	while (h != NULL)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This dir in the repo contains more codes utilising singly linked lists.
