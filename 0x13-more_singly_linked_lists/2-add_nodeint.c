d2b37e7385bd5dcaf4b1888b791104f9d476dd0a
Kali <kali@kali>
2022-10-25 16:24:06 -0400

    More code on singly linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,7 +8,9 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *temp;
+	listint_t *temp = malloc(sizeof(listint_t));
+	if (temp = NULL)
+		return (NULL);
 	int value = n;
 
 	temp->n = value;
