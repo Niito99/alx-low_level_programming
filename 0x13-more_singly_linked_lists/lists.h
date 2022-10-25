9d696e201703aec3e0b2ca121610be83338c7851
Kali <kali@kali>
2022-10-25 15:46:56 -0400

    More code on singly linked list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LIST_H
 #define LIST_H
 
+/* LIBRARIES */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,4 +19,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/* PROTOTYPES */
+size_t print_listint(const listint_t *h);
 #endif
