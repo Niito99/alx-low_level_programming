e02cb6230bd38e8b9fa024f4b277799248ac7974
Kali <kali@kali>
2022-11-04 16:01:23 -0400

    Read in and write a txt file to std output

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
 /* include Header files */
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 
 /* Prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
