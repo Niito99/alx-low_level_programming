1d7fbf8e1aa5f2588ff43bbbacb7b8be5910c5fe
Kali <kali@kali>
2022-11-04 16:42:45 -0400

    Read in and write a txt file to std output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
-/*
- * read_textfile - reads a text file and prints it to standard
- * output
+/**
+ * read_textfile - reads a text file and prints it to standard output
  * @filename: name of the file to read
  * @letters: number of letters and characters in the file
  * Return: number of letters read and print
@@ -23,12 +22,15 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buf == NULL)
 		return (0); /* check for malloc */
 
-	count = read(fd, buf, letters); /* the file descriptor is used to read the content of the file into a buffer buf */
+	count = read(fd, buf, letters);
+	/* the file descriptor is used to read the content of */
+	 /* the file into a buffer buf */
 
 	buf[letters] = '\0';
-	value = write(STDOUT_FILENO, buf, count); /* write the buffer to standard output */
+	value = write(STDOUT_FILENO, buf, count);
+	/* write the buffer to standard output */
 
-	if (count == -1 || value == -1 ||fd == -1)
+	if (count == -1 || value == -1 || fd == -1)
 	{
 		free(buf); /* free the memory allocated */
 		return (0);
