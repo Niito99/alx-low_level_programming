b74f10a8c258ac351983a04cd171faea65bacb72
Kali <kali@kali>
2022-11-04 15:42:40 -0400

    Read in and write a txt file to std output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,21 +12,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	size_t count, value;
-	char buf[letters];
+	char buf;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	count = letters;
+	fd = open(filename, O_RDONLY); /* open the file */
 
-	value = write(fd, buf, count);
-	if (value != count)
-		return (0);
-	write(1, buf, count);
+	buf = malloc(letters);	/* allocate memory to buffer */
+	if (buf == NULL)
+		return (0); /* check for malloc */
 
-	return (count);
+	count = read(fd, buf, letters); /* the file descriptor is used to read the content of the file into a buffer buf */
+
+	buf[letters] = '\0';
+	value = write(STDOUT_FILENO, buf, letters); /* write the buffer to standard output */
+	if (count == -1 || value == -1 ||fd == -1)
+	{
+		free(buf); /* free the memory allocated */
+		return (0);
+	}
+
+	close(fd);
+	free(buf);
+	return (value);
 
 }
