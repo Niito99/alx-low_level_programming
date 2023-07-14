2662b462f9b78de963fd08b864e8b9f516f09ae4
Niito99 <okinetheblessed@gmail.com>
2023-07-14 05:49:59 +0000

    Added printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints string
+ * Description: prints out a string to std output
+ *
+ * Return: value of zero
+ */
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", str);
+
+	return (0);
+}
