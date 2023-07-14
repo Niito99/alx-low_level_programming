d9dc458a333fd4e5c5f5e2963b71de1713eefce1
Niito99 <okinetheblessed@gmail.com>
2023-07-14 05:26:53 +0000

    out to stdout with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* main - prints string to stdout
+ * Return - returns a value of 0
+ */
+
+int main(){
+	char[] str = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return 0;
+}
